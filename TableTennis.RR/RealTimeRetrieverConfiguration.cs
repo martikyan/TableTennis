9ca70fe4deaa98f0cdc1cd40cbce135a735abc71
Karen Martikyan <karen.martikyan@digitain.com>
2020-04-20 23:59:14 +0400

    Removed redundant conf prop

--- TableTennis.RR/RealTimeRetrieverConfiguration.cs
+++ TableTennis.RR/RealTimeRetrieverConfiguration.cs
@@ -5,7 +5,6 @@ namespace TableTennis.RR
         public string BetsApiUrl { get; set; }
         public string BetsApiAccessToken { get; set; }
         public int MinimalHistoryCount { get; set; }
-        public double MinimalOddsDifference { get; set; }
         public int MaximalBigScoresPercentage { get; set; }
         public int ScanThresholdSeconds { get; set; }
         public int MaxGamesToRetrieve { get; set; }
